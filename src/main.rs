6ed65b90a0ad97d29319cdc2d2761f44173a6c42
michael-grunder <michael.grunder@gmail.com>
2020-01-02 11:52:44 -0800

    Switch order, add comment

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
-async fn test_scope_and_block() {
-    let not_copy = String::from("hello world!");
-    let not_copy_ref = &not_copy;
-
-    let (_, vals) = async_scoped::scope_and_block(|s| {
-        for _ in 0..10 {
-            let proc = || async {
-                assert_eq!(not_copy_ref, "hello world!");
-            };
-            s.spawn(proc());
-        }
-    });
-
-    assert_eq!(vals.len(), 10);
-}
+// The `scoped_futures` function fails to compile for me unless I uncomment the feature flag.
+// `test_scope_and_block` does compile, although I'm not clear on why.
+// #![feature(async_closure)]
 
 async fn scoped_futures() {
     let not_copy = String::from("hello world!");
@@ -27,6 +15,22 @@ async fn scoped_futures() {
             }
         })
     };
+}
+
+async fn test_scope_and_block() {
+    let not_copy = String::from("hello world!");
+    let not_copy_ref = &not_copy;
+
+    let (_, vals) = async_scoped::scope_and_block(|s| {
+        for _ in 0..10 {
+            let proc = || async {
+                assert_eq!(not_copy_ref, "hello world!");
+            };
+            s.spawn(proc());
+        }
+    });
+
+    assert_eq!(vals.len(), 10);
 }
 
 #[async_std::main]
